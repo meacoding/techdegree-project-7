efa66dddb085318ba50a598d2606686d11c597b1
meacoding <16675876+meacoding@users.noreply.github.com>
2018-12-03 16:55:44 -0500

    New members

--- js/app.js
+++ js/app.js
@@ -39,25 +39,61 @@ document.addEventListener("DOMContentLoaded", e => {
     }
   ];
 
-  //Excessive for demonstrating a mockup but a real project wouldn't be hard coded - in HTML or in JS.
-  const memberImg = (i, className) => {
+  //Excessive for demonstrating a mockup but a real project wouldn't be hard coded.
+  const memberImg = (i) => {
+    const className = `.member-img.member-${i}`;
     const member = document.querySelectorAll(className)[0];
     member.style.background = `yellow url('${
       members[i].img
     }') center center / cover no-repeat`;
   };
-  memberImg(0, ".member-img.member-0");
+  memberImg(0);
+  memberImg(1);
+  memberImg(2);
+  memberImg(3);
+  memberImg(4);
 
-  const memberName = (i, className) => {
+  const memberName = (i) => {
+    const className = `.member-name.member-${i}`;
     const member = document.querySelectorAll(className)[0];
     const name = `${members[i].firstName} ${members[i].lastName}`;
     member.textContent = name;
   };
-  memberName(0, ".member-name.member-0");
-});
+  memberName(0);
+  memberName(1);
+  memberName(2);
+  memberName(3);
+  memberName(4);
 
-const alert = document.querySelectorAll(".alert")[0];
-const hide = document.getElementById("hide");
-hide.addEventListener("click", e => {
+  const memberEmail = (i) => {
+    const className = `.member-email.member-${i}`;
+    const member = document.querySelectorAll(className)[0];
+    const email = `${members[i].email}`;
+    member.textContent = email;
+  };
+  memberEmail(1);
+  memberEmail(2);
+  memberEmail(3);
+  memberEmail(4);
+
+  const memberDate = (i) => {
+    const className = `.member-date.member-${i}`;
+    const member = document.querySelectorAll(className)[0];
+    const date = `${members[i].signupDate}`;
+    member.textContent = date;
+  };
+  memberDate(1);
+  memberDate(2);
+  memberDate(3);
+  memberDate(4);
+
+  //Hide alert message
+  const alert = document.querySelectorAll(".alert")[0];
+  const hide = document.getElementById("hide");
+  hide.addEventListener("click", e => {
   alert.style.display = "none";
 });
+});
+
+
+
